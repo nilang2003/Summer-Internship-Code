27a017f49268c87a808c8b1be06a4bfe683bb249
nilang2003 <110675577+nilang2003@users.noreply.github.com>
2024-03-20 10:56:16 +0530

    Create custome.js

---
+++ task-2/js/custome.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1.2,
+    spaceBetween: 30,
+
+    //   breakpoints: {
+    //     769: {
+    //       slidesPerView: 2,
+    //       slidesPerGroup: 2,
+    //     },
+    //   },
+    navigation: {
+        nextEl: ".swiper-button-next-custome",
+        prevEl: ".swiper-button-prev-custome",
+    },
+
+});
